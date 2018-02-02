30e8042c0fb2f167ce808412e783f8f70e0ee6b8
PRIORITY-LOCAL\kim.sy <sykim@presources.com.my>
2018-02-02 10:41:39 +0800

    Add project files.

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxInput = new System.Windows.Forms.TextBox();
+            this.msgLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBoxInput
+            // 
+            this.txtBoxInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxInput.Location = new System.Drawing.Point(22, 40);
+            this.txtBoxInput.Name = "txtBoxInput";
+            this.txtBoxInput.Size = new System.Drawing.Size(672, 80);
+            this.txtBoxInput.TabIndex = 0;
+            this.txtBoxInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // msgLabel
+            // 
+            this.msgLabel.AutoSize = true;
+            this.msgLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.msgLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msgLabel.Location = new System.Drawing.Point(19, 170);
+            this.msgLabel.Name = "msgLabel";
+            this.msgLabel.Size = new System.Drawing.Size(0, 55);
+            this.msgLabel.TabIndex = 1;
+            this.msgLabel.Click += new System.EventHandler(this.msgLabel_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 529);
+            this.Controls.Add(this.msgLabel);
+            this.Controls.Add(this.txtBoxInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxInput;
+        private System.Windows.Forms.Label msgLabel;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void msgLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {                      
+            try
+            {
+                double input = double.Parse(txtBoxInput.Text);
+                msgLabel.Text = $"10% of {input} is {0.1 * input}";
+            }
+            catch
+            {
+                //Regex r = new Regex("^[a-zA-Z]*$");
+                Match match = Regex.Match(txtBoxInput.Text, "^[a-zA-Z]*$");
+
+
+                if (txtBoxInput.Text == "" || match.Success == true )
+                {                   
+                    msgLabel.BackColor = Color.Azure;
+                    msgLabel.Text = "Please Enter a valid integer";
+                }
+            }            
+        }
+
+        private void msgLabel_Click_1(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
